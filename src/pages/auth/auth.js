46dd97c16950397bdfd45afc6d2a202c37e64695
Abrikosov Pavel <abrikosovp@mail.ru>
2021-10-18 19:50:21 +0300

    messanger

---
+++ src/pages/auth/auth.js
@@ -0,0 +1,17 @@
+(function () {
+
+    console.log("dasdasdasd")
+    // const form = document.forms[0];
+    //
+    // function onSubmit(event) {
+    //     event.preventDefault();
+    //
+    //     const formData = new FormData(form);
+    //     const values = Object.fromEntries(formData.entries());
+    //     console.log(values);
+    // }
+    //
+    // if(form){
+    //     form.addEventListener('submit', onSubmit)
+    // }
+})();
