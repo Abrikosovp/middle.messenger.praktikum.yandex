fc8e45a0941e3527af5d275dd073489dfe594735
Abrikosov Pavel <abrikosovp@mail.ru>
2022-02-21 11:57:17 +0300

    sprint_2 pug 2

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = 3000;
 // Все пути считаются относительно переменной __dirname
 // Подробнее про __dirname можно прочитать здесь https://nodejs.org/api/modules.html#modules_dirname
 app.use('/static', express.static(__dirname + '/public'));
+app.engine('pug', require('pug').__express);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','pug');
 
 
 app.listen(PORT, () => {
