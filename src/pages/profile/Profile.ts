9b9a399d26b3ddc3dd4de44b6c1af8f1ea2be1e6
Abrikosov Pavel <abrikosovp@mail.ru>
2022-05-12 17:12:17 +0300

    fix

--- src/pages/profile/Profile.ts
+++ src/pages/profile/Profile.ts
@@ -7,7 +7,7 @@ import {InputLabel, InputName, InputPlaceholder, UrlImg} from "../../utils/const
 import Avatar from "../../components/avatar";
 import Navigation from "./navigation";
 
-export class Profile extends Block {
+class Profile extends Block {
 
     constructor(props: TUserResponse) {
         super("div", ["form-page"], props);
