eca0cd2f27017b454d3bea0ecac5c3a922e97a1f
Abrikosov Pavel <abrikosovp@mail.ru>
2022-05-12 23:36:21 +0300

    fix import profile test

--- src/pages/profile/index.ts
+++ src/pages/profile/index.ts
@@ -1,6 +1,4 @@
-import {Profile} from './profile';
+import Profile from './profile';
 import {connect} from "../../modules/connect/connect";
-
-
 
 export default connect(Profile, ({user})=> user ?? {user: null});
